180dbdffd497d517de5f8d8f7e94e76ba75454e2
Kasper Peulen <kasperpeulen@gmail.com>
2014-09-24 14:05:26 +0200

    ...

--- Unicode/ctex_to_tex.js
+++ Unicode/ctex_to_tex.js
@@ -1,6 +1,46 @@
 /**
  * Created by kasperpeulen on 20/09/14.
  */
+
+var matchRecursive = function () {
+    var	formatParts = /^([\S\s]+?)\.\.\.([\S\s]+)/,
+        metaChar = /[-[\]{}()*+?.\\^$|,]/g,
+        escape = function (str) {
+            return str.replace(metaChar, "\\$&");
+        };
+
+    return function (str, format) {
+        var p = formatParts.exec(format);
+        if (!p) throw new Error("format must include start and end tokens separated by '...'");
+        if (p[1] == p[2]) throw new Error("start and end format tokens cannot be identical");
+
+        var	opener = p[1],
+            closer = p[2],
+        /* Use an optimized regex when opener and closer are one character each */
+            iterator = new RegExp(format.length == 5 ? "["+escape(opener+closer)+"]" : escape(opener)+"|"+escape(closer), "g"),
+            results = [],
+            openTokens, matchStartIndex, match;
+
+        do {
+            openTokens = 0;
+            while (match = iterator.exec(str)) {
+                if (match[0] == opener) {
+                    if (!openTokens)
+                        matchStartIndex = iterator.lastIndex;
+                    openTokens++;
+                } else if (openTokens) {
+                    openTokens--;
+                    if (!openTokens)
+                        results.push(str.slice(matchStartIndex, match.index));
+                }
+            }
+        } while (openTokens && (iterator.lastIndex = matchStartIndex));
+
+        return results;
+    };
+}();
+
+
 var ctex_to_tex = function (){
     $('#latex').val($('#ta').val());
     var textarea = $('#latex').val();
@@ -12,9 +52,9 @@ var ctex_to_tex = function (){
 
 
 
-    while (binreg("∕").test(textarea)){
-        textarea = textarea.replace(binreg("∕"),'\\frac{$1$2}{$3$4}');
-    }
+    //while (binreg("∕").test(textarea)){
+        //textarea = textarea.replace(binreg("∕"),'\\frac{$1$2}{$3$4}');
+    //}
     textarea = textarea.replace(binreg("¦"),'\\binom{$1$2}{$3$4}');
     textarea = textarea.replace(binreg("↖"),'\\overset{$3$4}{$1$2}');
     textarea = textarea.replace(binreg("↙"),'\\underset{$3$4}{$1$2}');
@@ -22,7 +62,6 @@ var ctex_to_tex = function (){
 
     var text = textarea.split('');
     for (var index = 0; index < text.length; index++) {
-        var i =index;
         if (text[index] === "⁅") {
             var sub=  textarea.substring(index);
             var endindex = sub.indexOf("⁆");
@@ -49,6 +88,52 @@ var ctex_to_tex = function (){
             }
 
         }
+        var i = index;
+        var before, after, m;
+        if (text[i] === "∕") {
+            if (text[i - 1] === "}") {
+                m = matchRecursive(textarea.substring(0, i), "{...}");
+                before = m[m.length - 1];
+            }
+            else if (text[i - 1] === ")") {
+                m = matchRecursive(textarea.substring(0, i), "(...)");
+                before = m[m.length - 1];
+            }
+            else if (text[i - 1] === "]") {
+                m = matchRecursive(textarea.substring(0, i), "[...]");
+                before = m[m.length - 1];
+            }
+            else {
+                before = text[i - 1];
+            }
+            if (text[i + 1] === "{") {
+                after = matchRecursive(textarea.substring(i + 1), "{...}")[0];
+            }
+            else if (text[i + 1] === "(") {
+                after = matchRecursive(textarea.substring(i + 1), "(...)")[0];
+            }
+            else if (text[i + 1] === "[") {
+                after = matchRecursive(textarea.substring(i + 1), "[...]")[0];
+            }
+            else if (text[i+1] === "⁆") {
+                after = "";
+            }
+            else {
+                after = text[i+1];
+            }
+            if (before && after) {
+
+            before = before.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+            after = after.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+
+            console.log(before, after);
+            var re = new RegExp("[{(\\[]?(" + before + ")[})\\]]?∕[{(\\[]?(" + after + ")[\\]})]?", "g");
+            textarea = textarea.replace(re, "\\frac{$1}{$2}");
+            text = textarea.split('');
+            index = 0;
+            }
+        }
+
         textarea = text.join('');
     }
 
@@ -93,10 +178,10 @@ var ctex_to_tex = function (){
         textarea = textarea.replace(reg,subscripts[a]);
     }
 
-    textarea = textarea.replace(/(?:\^[\d\w\=\+\-\→]+)+/g, function($1) {
+    textarea = textarea.replace(/(?:\^[\d\w\=\→]+)+/g, function($1) {
         return "^{"+$1.replace(/\^/g,"")+"}";
     });
-    textarea = textarea.replace(/(?:_[\d\w\=\+\-\→]+)+/g, function($1) {
+    textarea = textarea.replace(/(?:_[\d\w\=\→]+)+/g, function($1) {
         return "_{"+$1.replace(/_/g,"")+"}";
     });
     textarea = textarea.replace(/"(\w+)"(\(|\[|\{)/g,'\\mathop{\\mathrm{$1}}$2');
